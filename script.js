5868e3c2937fb74aa8f1196d51ee6b3fd2fe4ea6
Vamshi Muluguri <vamshimulgoori2001@gmail.com>
2021-10-15 15:29:49 +0530

    designed login page with sawo labs

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert("hello");     
+
+var config = {
+
+    containerID: "sawo-container",
+
+    identifierType: "email",
+
+    apiKey: "a1a7cce9-95ee-4bda-940e-32f03d29b73e",
+
+    onSuccess: (payload) => {
+        window.location = "./index.html"
+    },
+};
+var sawo = new Sawo(config);
+sawo.showForm(); 
+
